16a96ddf4305c66f7b76b4f919649c3c420be878
Narmada Muralikrishnan <71222021+letsbecometechie@users.noreply.github.com>
2022-09-04 12:54:59 +0530

    Time: 1 ms (97.67%), Space: 53.8 MB (71.52%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -0,0 +1,28 @@
+// TC O(N*M +N*M) SC O(N)+ O(M)
+class Solution {
+
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length, column = matrix[0].length;
+        int col0 = -1;
+
+        for (int i = 0; i < row; i++) {
+            if(matrix[i][0]==0) col0=0;
+            for (int j = 1; j < column; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0]=0; matrix[0][j]=0;
+                }
+            }
+        }
+
+        for (int i=row-1; i>=0; i--) {
+            for (int j=column-1; j>=1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+            if(col0==0){
+                matrix[i][0]=0;
+            }
+        }
+    }
+}
