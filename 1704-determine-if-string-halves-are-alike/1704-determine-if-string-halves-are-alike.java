d780f8ea54504b343eb9e2e9018fd358783671dd
Narmada Muralikrishnan <71222021+letsbecometechie@users.noreply.github.com>
2022-12-01 19:58:28 +0530

    Time: 14 ms (26.55%), Space: 42 MB (72.17%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,37 +1,18 @@
 class Solution {
-   public static boolean halvesAreAlike(String s) {
 
-        int length = s.length();
+    public static boolean halvesAreAlike(String s) {
+        HashSet<Character> set = new HashSet<>(Arrays.asList('a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'));
 
-        int c1= countVowels(s.substring(0, length/2));
-        int c2 = countVowels(s.substring(length/2, length));
+        int c1 = 0, c2 = 0;
 
-        return c1==c2;
-    }
-
-    private static int countVowels(String s){
-        int count=0;
-        for(int i=0;i<s.length();i++){
-            switch (s.charAt(i)){
-                case 'a':
-                case 'A':
-
-                case 'e':
-                case 'E':
-
-                case 'i':
-                case 'I':
-
-                case 'o':
-                case 'O':
-
-                case 'u':
-                case 'U':
-                    count++;
-                    break;
-            }
-
+        for (int i = 0; i < s.length() / 2; i++) {
+            c1 += set.contains(s.charAt(i)) ? 1 : 0;
         }
-        return count;
+
+        for (int i = s.length() / 2; i < s.length(); i++) {
+            c2 += set.contains(s.charAt(i)) ? 1 : 0;
+        }
+
+        return c1 == c2;
     }
 }
